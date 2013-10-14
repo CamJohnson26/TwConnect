0e977d59d7a8856a72e4e4404212e14ca407338d
Cameron Johnson <cameronrossjohnson@gmail.com>
2013-10-14 11:22:14 -0500

    Initial Code Update

---
+++ djangosite/polls/views.py
@@ -0,0 +1,8 @@
+# Create your views here.
+from django.http import HttpResponse
+import datetime
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
